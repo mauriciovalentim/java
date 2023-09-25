d020b199a56d8c2d3bf001be6de065fcfc467806
Mauricio Valentim <mauriciovalentim.dev@gmail.com>
2023-09-25 14:27:10 -0300

    Basic commands

---
+++ 01 - java-anatomia-classes/src/edu/mauricio/primeiraSemana/MyClass.java
@@ -0,0 +1,11 @@
+package edu.mauricio.primeiraSemana;
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println(nomeCompleto("Mauricio", "Valentim"));
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome + " " + segundoNome;
+    }
+
+}
