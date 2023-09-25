7a7fb9653e7cc71ddddd70818f0ee77b9d01191a
Mauricio Valentim <mauriciovalentim.dev@gmail.com>
2023-09-25 14:57:11 -0300

    types and variables

---
+++ 02 - tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        // TiposEVariaveis.java
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // Constantes
+        final double NUMERO_PI = 3.14;
+    }
+}
