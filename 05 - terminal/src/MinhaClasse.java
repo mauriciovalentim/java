f0e9d5c976c2d248d632b395a82987f80aab340e
mauriciovalentim <m.junior.souza2000@gmail.com>
2023-09-30 15:19:06 -0300

    Learning about Scanner

--- 05 - terminal/src/MinhaClasse.java
+++ 05 - terminal/src/MinhaClasse.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Olá, estou executando o código");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Olá, eu sou o " + nome + " " + sobrenome + " tenho " + altura + " de altura e " + idade + " anos");
     }
 }
